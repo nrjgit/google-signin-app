4fc0562eec55e8e2b252817332c8dee887acedd7
nrjgit <mail.nrj@gmail.com>
2025-03-10 17:20:05 +0530

    CROSS ORIGIN ADDED in server .js

--- server.js
+++ server.js
@@ -18,5 +18,12 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 })
 
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 module.exports = app;
 
