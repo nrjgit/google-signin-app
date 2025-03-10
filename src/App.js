a7415169a128ab83c142c0357de4f0b0e0c72672
nrjgit <mail.nrj@gmail.com>
2025-03-10 15:11:17 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID; // Use your Client ID from .env
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log('Login Success:', credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </GoogleOAuthProvider>
   );
 }
 
